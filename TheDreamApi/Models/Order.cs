d559a20045fdc8a7a7a60ed3ec5218e60744b05a
mayazini <72668921+mayazini@users.noreply.github.com>
2023-06-17 22:06:17 +0300

    finale version

--- TheDreamApi/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheDreamApi.Models
-{
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public int TotalAmount { get; set; }
-        public string UserName { get; set; }
-    }
-}
